c4e8a37032551e5d030fe04bfc9b185351800eb7
Kania <kanialdo@gmail.com>
2017-11-11 22:40:45 +0100

    Add Matches and Players placeholder

--- app/src/main/java/pl/krystiankaniowski/billonsport/ui/MainActivity.kt
+++ app/src/main/java/pl/krystiankaniowski/billonsport/ui/MainActivity.kt
@@ -1,10 +1,12 @@
 package pl.krystiankaniowski.billonsport.ui
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.github.salomonbrys.kodein.KodeinInjected
 import com.github.salomonbrys.kodein.KodeinInjector
 import com.github.salomonbrys.kodein.android.appKodein
+import kotlinx.android.synthetic.main.activity_main.*
 import pl.krystiankaniowski.billonsport.R
 
 class MainActivity : AppCompatActivity(), KodeinInjected {
@@ -15,6 +17,23 @@ class MainActivity : AppCompatActivity(), KodeinInjected {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 		inject(appKodein())
+
+		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+	}
+
+	private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+		when (item.itemId) {
+			R.id.navigation_players -> {
+				message.setText(R.string.title_players)
+				return@OnNavigationItemSelectedListener true
+			}
+			R.id.navigation_matches -> {
+				message.setText(R.string.title_matches)
+				return@OnNavigationItemSelectedListener true
+			}
+		}
+		false
 	}
 
 }
